06079256bfc04ad8d7c6001972cd4afd5077ce81
ashuvinayak1508 <ashuvinayak1508@gmail.com>
2023-06-10 00:52:09 +0530

    first commit

---
+++ sricpt.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings: ["CORPORATE EVENTS","EDUCATIONAL EVENTS","MARRIGES","PARTIES"],
+    typespeed: 0.0090,
+    backspeed: 10,
+    backDelay:5000,
+    typeDelay:5000,
+    loop: true 
+});
